5e5f3071c2cadbfac60bcd89a1307b4d48135544
Arthur Judd <amjudd315@gmail.com>
2022-02-18 11:13:10 -0700

    added more realistic initial transfers

--- src/App.js
+++ src/App.js
@@ -21,12 +21,13 @@ const ForecastService = require("./core/forecastService");
 // INITIAL DATA
 
 let initialAccounts = [
-  new Account("checking",20000),
-  new Account("savings",1500),
+  new Account("checking",500),
+  new Account("savings",20000),
 ];
 
 
 let scheduledTransactions = [
+  // Recurring Expenses
   new TransactionSchedule( // Groceries
       new TransactionTemplate(TransactionType.Expense,"Groceries",200,"checking", null, 'food'),
       new XPerMonthSchedule(2, new Date(2021,MONTHS.APR,10))
@@ -47,41 +48,59 @@ let scheduledTransactions = [
       new TransactionTemplate(TransactionType.Expense,"Child Care",12,"checking", null),
       new XPerMonthSchedule(1, new Date(2021,MONTHS.APR,10))
   ),
-  new TransactionSchedule( // Clozd parttime (should actually be every 2 weeks)
-      new TransactionTemplate(TransactionType.Income,"Student Job PT",1000,"checking", null),
-      new XPerMonthSchedule(2, new Date(2022,MONTHS.FEB,15), new Date(2022,MONTHS.MAY,5))
-  ),
   
   new TransactionSchedule(
-    new TransactionTemplate(TransactionType.Expense,"Graduation Party",100,"checking", null),
-    new OneTimeSchedule(new Date(2022,MONTHS.APR,22))
-  ),
-
-  new TransactionSchedule( // Clozd fulltime
-      new TransactionTemplate(TransactionType.Income,"Full TIme Salary",2500,"checking", null),
-      new XPerMonthSchedule(2, new Date(2022,MONTHS.MAY,15))
-  ),
-  new TransactionSchedule(
-      new TransactionTemplate(TransactionType.Expense,"Rent",550,"checking", null),
-      new XPerMonthSchedule(1, new Date(2020,MONTHS.FEB,5), new Date(2022,MONTHS.JUL,5))
+    new TransactionTemplate(TransactionType.Expense,"Rent",550,"checking", null),
+    new XPerMonthSchedule(1, new Date(2020,MONTHS.FEB,5), new Date(2022,MONTHS.JUL,5))
   ),
   new TransactionSchedule(
       new TransactionTemplate(TransactionType.Expense,"Mortgage",2000,"checking", null),
       new XPerMonthSchedule(1, new Date(2022,MONTHS.AUG,5))
   ),
+  
+
+  // Recurring Income Sources
+  new TransactionSchedule( // Clozd parttime (should actually be every 2 weeks)
+      new TransactionTemplate(TransactionType.Income,"Student Job PT",1000,"checking", null),
+      new XPerMonthSchedule(2, new Date(2022,MONTHS.FEB,15), new Date(2022,MONTHS.MAY,5))
+  ),
+  new TransactionSchedule( // Clozd fulltime
+    new TransactionTemplate(TransactionType.Income,"Full TIme Salary",2500,"checking", null),
+    new XPerMonthSchedule(2, new Date(2022,MONTHS.MAY,15))
+  ),
+
+
+  // Recurring Transfers
   new TransactionSchedule(
-      new TransactionTemplate(TransactionType.Expense,"Down Payment",20000,"checking", null),
+    new TransactionTemplate(TransactionType.Transfer,"Transfer",750,"savings","checking"),
+    new XPerMonthSchedule(1, new Date(2022,MONTHS.JAN,5), new Date(2022,MONTHS.MAY,5))
+  ),
+  new TransactionSchedule(
+    new TransactionTemplate(TransactionType.Transfer,"Transfer",1700,"savings","checking"),
+    new XPerMonthSchedule(2, new Date(2022,MONTHS.MAY,15), new Date(2022,MONTHS.JUL,30))
+  ),
+  new TransactionSchedule(
+    new TransactionTemplate(TransactionType.Transfer,"Transfer",1000,"savings","checking"),
+    new XPerMonthSchedule(2, new Date(2022,MONTHS.AUG,15))
+  ),
+
+
+  // One-Time Transactions
+  new TransactionSchedule(
+    new TransactionTemplate(TransactionType.Expense,"Graduation Party",100,"checking", null),
+    new OneTimeSchedule(new Date(2022,MONTHS.APR,22))
+  ),
+  new TransactionSchedule(
+      new TransactionTemplate(TransactionType.Expense,"Down Payment",20000,"savings", null),
       new OneTimeSchedule(new Date(2022,MONTHS.JUL,5))
   ),
   new TransactionSchedule(
-      new TransactionTemplate(TransactionType.Expense,"Closing Costs",10000,"checking", null),
+      new TransactionTemplate(TransactionType.Expense,"Closing Costs",10000,"savings", null),
       new OneTimeSchedule(new Date(2022,MONTHS.JUL,5))
-  ),
-  new TransactionSchedule(
-      new TransactionTemplate(TransactionType.Transfer,"Transfer to Savings",750,"savings","checking"),
-      new XPerMonthSchedule(2, new Date(2022,MONTHS.OCT,5))
   ),
 ]
+
+
 
 let initialCategories = [
   new Category('food', 'Food')
