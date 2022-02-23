43991de7c712a242152f2f56761175f615fbab8d
Tyla Merrill <tylamerrill@gmail.com>
2022-02-23 15:21:54 -0700

    Add menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+const { Link } = require("react-router-dom");
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <Link to='/'></Link>
+    </div>
+  );
+};
+
+export default Menu;
